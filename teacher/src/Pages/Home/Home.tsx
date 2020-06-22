d06b72984b6c102c0414f8f8ab4cb3fa1c2c8aa4
bertanbee <bertan1707@gmail.com>
2020-06-22 19:27:07 -0300

    Deleting test image and clearing div for the next task

--- teacher/src/Pages/Home/Home.tsx
+++ teacher/src/Pages/Home/Home.tsx
@@ -3,13 +3,11 @@ import './styles.css';
 
 import Card from '../../Components/Card';
 
-import img from './img.png'
-
 function Home() {
     return (
         <div className="div">
             <div className="img-content">
-                <img src={img} alt="img"></img>
+                
             </div>
             
             <div className="selector">
